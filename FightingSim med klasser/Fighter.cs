80c1ddc7918b2ec8bfa53298000829d156775f7b
NoCookies4You <32069171+NoCookies4You@users.noreply.github.com>
2019-10-17 16:27:29 +0200

    Add project files.

---
+++ FightingSim med klasser/Fighter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightingSim_med_klasser
+{
+    class Fighter
+    {
+
+        private int hp;
+        public string name;
+
+        static Random rndNumber = new Random();
+
+        public int Attack()
+        {
+            return rndNumber.Next(10, 40);
+        }
+
+        public void Hurt(int damage)
+        {
+            hp = hp - damage;
+        }
+
+        public bool IsAlive()
+        {
+            if(hp > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int GetHp()
+        {
+            return hp;
+        }
+
+        public Fighter(string selectedName)
+        {
+            hp = rndNumber.Next(75, 150);
+            name = selectedName;
+        }
+            
+    }
+}
---
+++ FightingSim med klasser/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightingSim_med_klasser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a name for player 1");
+            string typedName = Console.ReadLine();
+            Fighter a = new Fighter(typedName);
+
+
+
+            Console.WriteLine("Type a name for player 2");
+            typedName = Console.ReadLine();
+            Fighter b = new Fighter(typedName);
+
+            Console.WriteLine(a.name + "'s hp is: " + a.GetHp());
+            Console.WriteLine(b.name + "'s hp is: " + b.GetHp());
+
+
+            Console.ReadLine();
+
+            bool playing = true;
+            while (playing == true)
+            {
+                b.Hurt(a.Attack());
+
+                a.Hurt(b.Attack());
+                Console.WriteLine(a.name + "'s hp is: " + a.GetHp());
+                Console.WriteLine(b.name + "'s hp is: " + b.GetHp());
+
+                if (a.IsAlive() == false || b.IsAlive() == false)
+                {
+                    playing = false;
+                }
+                Console.ReadLine();
+            }
+            if (a.IsAlive() == true)
+            {
+                Console.WriteLine(a.name + " vann!");
+            }
+            else if (b.IsAlive() == true)
+            {
+                Console.WriteLine(b.name + " vann!");
+            }
+            else
+            {
+                Console.WriteLine("Båda dog. Gj");
+            }
+            Console.ReadLine();
+        }
+    }
+}
